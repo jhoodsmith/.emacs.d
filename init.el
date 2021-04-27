7905a371a05670e700c8fa80570fed501be8bf6b
James Hood-Smith <james@hood-smith.co.uk>
2021-04-27 18:57:22 +0100

    ✅ Made iPython default shell

--- init.el
+++ init.el
@@ -277,8 +277,12 @@
   :config
   (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup))
 
-;; (use-package pytest)
 (use-package python-pytest)
+
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt --InteractiveShell.display_page=True")
+
+
 ;; Yasnippet
 (use-package yasnippet
   :diminish yas-minor-mode
